94b36d714ba921e11a8d1b16a461b37ef38cddd4
Evangelis Albesa <145369477+evangelisqa@users.noreply.github.com>
2024-09-14 14:57:24 -0700

    add

---
+++ scripts/myhome.js
@@ -0,0 +1,11 @@
+// Output the current year in the footer's first paragraph
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Output the date the document was last modified in the footer's second paragraph
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
+
+//document.addEventListener('DOMContentLoaded', function(){
+ // alert('Welcome to Our Cards Website!');
+//});
